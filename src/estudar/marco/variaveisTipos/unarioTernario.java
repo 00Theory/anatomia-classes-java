f3e3683b307773a68613b377a7fa6507475622d3
00Theory <marco200002010@outlook.com>
2023-07-23 17:12:47 -0300

    teste de unarios

---
+++ src/estudar/marco/variaveisTipos/unarioTernario.java
@@ -0,0 +1,11 @@
+package estudar.marco.variaveisTipos;
+
+public class UnarioTernario{
+    public static void main(String[] args) {
+        int numero = 2;
+        System.out.println(numero);
+        System.out.println(++numero);
+        System.out.println(--numero);
+        System.out.println(++numero + --numero);
+    }
+}
