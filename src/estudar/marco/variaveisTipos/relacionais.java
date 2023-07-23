f6c90b50e4c9281e74b2fc48e696c797e4bd510b
00Theory <marco200002010@outlook.com>
2023-07-23 19:12:10 -0300

    relacionais e verificação de conteúdo em boolean

--- src/estudar/marco/variaveisTipos/relacionais.java
+++ src/estudar/marco/variaveisTipos/relacionais.java
@@ -1,5 +1,24 @@
 package estudar.marco.variaveisTipos;
 
 public class relacionais {
-    
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numero1 é igual a numero2? " + simNao);
+
+        simNao = numero1 != numero2;
+        System.out.println("numeroUm é diferente do numeroDois? " + simNao + "\n");
+        System.out.println("comparação de conteúdos:\n");
+
+        String a = "marquin";
+        String b = new String("marquin");
+
+        boolean verificarNome = a == b;
+
+        System.out.println(verificarNome);
+        System.out.println(a.equals(b));
+    }
 }
