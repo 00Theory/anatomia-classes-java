fe857418d8cb724dc233da9b29667f0bf14075ec
00Theory <marco200002010@outlook.com>
2023-07-23 17:06:30 -0300

    variaveis e tipos com concatenacao, unarios e ternarios

--- src/estudar/marco/variaveisTipos/TiposVariaveis.java
+++
@@ -1,5 +0,0 @@
-package estudar.marco.variaveisTipos;
-
-public class TiposVariaveis {
-    
-}
---
+++ src/estudar/marco/variaveisTipos/TiposVariaveisConcatenacao.java
@@ -0,0 +1,13 @@
+package estudar.marco.variaveisTipos;
+
+public class TiposVariaveisConcatenacao {
+    public static void main(String[] args) {
+        System.out.println(1+1+1+ "1" + (1+1));
+        System.out.println(1+1+"1"+1+1);
+        System.out.println(1+"1"+1+"1");
+        System.out.println("1"+1+1+1);
+        System.out.println("1"+(1+1+1));
+    }
+
+    
+}
