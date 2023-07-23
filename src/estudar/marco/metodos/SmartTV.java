6bff494afcdee4dda7b80e226e25b876f5d6d034
00Theory <marco200002010@outlook.com>
2023-07-23 20:44:54 -0300

    Documentação

--- src/estudar/marco/metodos/SmartTV.java
+++ src/estudar/marco/metodos/SmartTV.java
@@ -2,6 +2,23 @@ package estudar.marco.metodos;
 
 import java.util.random.RandomGenerator;
 
+/**
+ * 
+ * <h1>SmartTV</h1>
+ * A SmartTv pode ser desligada ou ligada, além de se poder
+ * aumentar ou diminuir o volume dela, também podendo ter os
+ * canais dela alterados.
+ * 
+ * <h2>Métodos:</h2>
+ * <h3>LigarTv</h3>
+ * <h3>DesligarTv</h3>
+ * <h3>AumentarVolume</h3>
+ * <h3>DiminuirVolume</h3>
+ * 
+ * @author Marco Lima
+ * @version 1.0
+ * @since 23/07/2023
+ */
 public class SmartTV {
 
     boolean ligada = false;
@@ -10,6 +27,10 @@ public class SmartTV {
 
     public void LigarTv(){
         ligada = true;
+    }
+
+    public void DesligarTV(){
+        ligada = false;
     }
 
     public void AumentarVolume(){
@@ -21,6 +42,7 @@ public class SmartTV {
     }
 
     /**
+     * <h2>TrocarCanal</h2>
      * Este método TrocarCanal é utilizado para alterar a variável
      * canal por incrementos ou decrementos gerados de maneira
      * aleatória.
