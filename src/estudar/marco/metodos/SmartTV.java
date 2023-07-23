739ec34d2be39fe9ef321bad5fbac0782340c353
00Theory <marco200002010@outlook.com>
2023-07-23 20:32:28 -0300

    Smart TV Metodos

---
+++ src/estudar/marco/metodos/SmartTV.java
@@ -0,0 +1,27 @@
+package estudar.marco.metodos;
+
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+public class SmartTV {
+
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 50;
+
+    public void LigarTv(){
+        ligada = true;
+    }
+
+    public void AumentarVolume(){
+        volume += 5;
+    }
+
+    public void DiminuirVolume(){
+        volume -= 5;
+    }
+
+    public void TrocarCanal(){
+        canal += RandomGenerator.getDefault().nextInt(-1, 2);
+    }
+}
---
+++ src/estudar/marco/metodos/UsuarioMetodos.java
@@ -0,0 +1,31 @@
+package estudar.marco.metodos;
+
+public class UsuarioMetodos {
+    public static void main(String[] args) {
+        SmartTV smartTV = new SmartTV();
+
+        smartTV.LigarTv();
+
+        System.out.println("TV está ligada? " + smartTV.ligada);
+
+        smartTV.TrocarCanal();
+        System.out.println("Canal Atual: " + smartTV.canal);
+        smartTV.TrocarCanal();
+        System.out.println("Canal Atual: " + smartTV.canal);
+        smartTV.TrocarCanal();
+        System.out.println("Canal Atual: " + smartTV.canal);
+        smartTV.TrocarCanal();
+        System.out.println("Canal Atual: " + smartTV.canal);
+        smartTV.TrocarCanal();
+        System.out.println("Canal Atual: " + smartTV.canal);
+        smartTV.TrocarCanal();
+        System.out.println("Canal Atual: " + smartTV.canal);
+        smartTV.TrocarCanal();
+        System.out.println("Canal Atual: " + smartTV.canal);
+        smartTV.TrocarCanal();
+        System.out.println("Canal Atual: " + smartTV.canal);
+        smartTV.TrocarCanal();
+        System.out.println("Canal Atual: " + smartTV.canal);
+    }
+    
+}
