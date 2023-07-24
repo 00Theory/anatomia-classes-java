d6c4adf95d1fd7f131a3b14d14e001cc269601da
00Theory <marco200002010@outlook.com>
2023-07-23 21:10:20 -0300

    javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"estudar.marco.metodos","l":"SmartTV"},{"p":"estudar.marco.metodos","l":"UsuarioMetodos"}];updateSearchResults();
