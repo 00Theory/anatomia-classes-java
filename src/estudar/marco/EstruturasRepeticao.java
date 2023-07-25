fc0a9177beee5b32544bcfda213c081b5dca19df
00Theory <marco200002010@outlook.com>
2023-07-25 12:52:59 -0300

    Repetição com Continue

--- src/estudar/marco/EstruturasRepeticao.java
+++ src/estudar/marco/EstruturasRepeticao.java
@@ -3,12 +3,12 @@ package estudar.marco;
 public class EstruturasRepeticao{
     public static void main(String[] args) {
         for(int numero = 1; numero <=5; numero ++){
-            System.out.println(numero);
+            
 		if(numero==3){
-            System.out.println("PAROU");
-		    break;
+            System.out.println("PULAR O " + numero);
+		    continue;
         }
-			
+			System.out.println(numero);
 		
 		
 	}
