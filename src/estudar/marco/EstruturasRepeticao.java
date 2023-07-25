0e3f785d4e52735b7104550ba04bbef313f9a7ad
00Theory <marco200002010@outlook.com>
2023-07-25 12:50:26 -0300

    For-Each

---
+++ src/estudar/marco/EstruturasRepeticao.java
@@ -0,0 +1,12 @@
+package estudar.marco;
+
+public class EstruturasRepeticao{
+    public static void main(String[] args) {
+        String alunos [] =  {"FELIPE","JONAS","JULIA","MARCOS"};
+	
+        //Forma abreviada
+	    for(String aluno : alunos) {
+	        System.out.println(aluno);
+	    }
+    }
+}
