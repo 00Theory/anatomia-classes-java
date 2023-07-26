59412340b10da8b4f8b07324b980fd8d476062e6
00Theory <marco200002010@outlook.com>
2023-07-26 20:34:47 -0300

    Estudo de Enum 2

--- src/estudar/poo/EstadoBrasileiro.java
+++ src/estudar/poo/EstadoBrasileiro.java
@@ -1,19 +1,21 @@
 package estudar.poo;
 
 public enum EstadoBrasileiro{
-    SAO_PAULO ("SP","São Paulo"),
-	RIO_JANEIRO ("RJ", "Rio de Janeiro"),
-	PIAUI ("PI", "Piauí"),
-	MARANHAO ("MA","Maranhão")
+    SAO_PAULO ("SP","São Paulo", 10),
+	RIO_JANEIRO ("RJ", "Rio de Janeiro", 86),
+	PIAUI ("PI", "Piauí", 01),
+	MARANHAO ("MA","Maranhão", 39)
     
     ;
 	
 	private String nome;
 	private String sigla;
-	
-	EstadoBrasileiro(String sigla, String nome) {
+	private int ibge;
+
+	EstadoBrasileiro(String sigla, String nome, int ibge) {
 		this.sigla = sigla;
 		this.nome = nome;
+		this.ibge = ibge;
 	}
 	public String getSigla() {
 		return sigla;
@@ -23,5 +25,9 @@ public enum EstadoBrasileiro{
 	}
 	public String getNomeMaiusculo() {
 		return nome.toUpperCase();
+	}
+
+	public int getIbge(){
+		return ibge;
 	}
 }
--- src/estudar/poo/SistemaIbge.java
+++ src/estudar/poo/SistemaIbge.java
@@ -3,7 +3,13 @@ package estudar.poo;
 public class SistemaIbge{
     public static void main(String[] args) {
         for(EstadoBrasileiro e: EstadoBrasileiro.values()){
-            System.out.println(e.getSigla() + " - " + e.getNome());
+            System.out.println(e.getSigla() + " - " + e.getNome() + " - " + e.getIbge());
         }
+
+        EstadoBrasileiro estado = EstadoBrasileiro.PIAUI;
+        System.out.println(estado.getNome());
+        System.out.println(estado.getSigla());
+        System.out.println(estado.getNomeMaiusculo());
+        System.out.println(estado.getIbge());
     }
 }
