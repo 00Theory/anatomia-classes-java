b42eaa00b4a1866671df716e3896fa60051b4331
00Theory <marco200002010@outlook.com>
2024-04-22 21:41:19 -0300

    Árvore Binária - método de exibição de resultados em InOrdem, PréOrdem e PósOrdem.

--- src/EstruturaDados/arvores/ArvoreBinaria.java
+++ src/EstruturaDados/arvores/ArvoreBinaria.java
@@ -8,6 +8,8 @@ public class ArvoreBinaria<T extends Comparable<T>> {
         this.raiz = null;
     }
 
+    // <------- INSERIR ------->
+
     public void inserir(T conteudo){
         BinNo<T> novoNo = new BinNo<T>(conteudo);
         raiz = inserir(raiz, novoNo);
@@ -27,4 +29,44 @@ public class ArvoreBinaria<T extends Comparable<T>> {
         return atual;
     }
 
+    // <------- EXIBIÇÃO ------->
+
+    public void exibirInOrdem(){
+        System.out.println(" Exibindo InOrdem");
+        exibirInOrdem(this.raiz);
+    }
+
+    private void exibirInOrdem(BinNo<T> atual){
+        if (atual != null){
+            exibirInOrdem(atual.getNoEsq());
+            System.out.println(atual.getConteudo() + ", ");
+            exibirInOrdem(atual.getNoDir());
+        }
+    }
+
+    public void exibirPosOrdem(){
+        System.out.println(" Exibindo PósOrdem");
+        exibirPosOrdem(this.raiz);
+    }
+
+    private void exibirPosOrdem(BinNo<T> atual){
+        if (atual != null){
+            exibirPosOrdem(atual.getNoEsq());
+            exibirPosOrdem(atual.getNoDir());
+            System.out.println(atual.getConteudo() + ", ");
+        }
+    }
+
+    public void exibirPreOrdem(){
+        System.out.println(" Exibindo PréOrdem");
+        exibirPreOrdem(this.raiz);
+    }
+
+    private void exibirPreOrdem(BinNo<T> atual){
+        if (atual != null){
+            System.out.println(atual.getConteudo() + ", ");
+            exibirPreOrdem(atual.getNoEsq());
+            exibirPreOrdem(atual.getNoDir());
+        }
+    }
 }
